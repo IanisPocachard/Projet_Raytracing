f12c4a08b56c66336d9b7c4f0b40ed73169f27c7
Adrien Tritarelli <atritarelli@outlook.com>
2026-06-08 17:55:51 +0200

    modif rapide envoyer calcul avec import de RemoteException mais non gestion car je ne sais pas comment Ambroise veut les gérer

--- client/EnvoyerCalcul.java
+++ client/EnvoyerCalcul.java
@@ -1,20 +1,23 @@
 package client;
 
+import calcul.InterfaceNoeudDeCalcul;
+import java.rmi.RemoteException;
 import raytracer.*;
 
 class EnvoyerCalcul extends Thread {
-  private TacheCalcul calcul;
-  private Disp disp;
-  private InterfaceNoeudDeCalcul noeud;
-  
-  public EnvoyerCalcul (int x0, int y0, int l, int h, Scene s, Disp d, InterfaceNoeudDeCalcul n) {
-    this.calcul = new TacheCalcul(s, x0, y0, l, h);
-    this.disp = d;
-    this.noeud = n;
-  }
-  
-  public void run() {
-    this.disp.setImage(this.noeud.calculer(this.calcul), this.calcul.getX(), this.calcul.getY());
-  }
+
+    private TacheCalcul calcul;
+    private Disp disp;
+    private InterfaceNoeudDeCalcul noeud;
+
+    public EnvoyerCalcul(int x0, int y0, int l, int h, Scene s, Disp d, InterfaceNoeudDeCalcul n) {
+        this.calcul = new TacheCalcul(s, x0, y0, l, h);
+        this.disp = d;
+        this.noeud = n;
+    }
+
+    public void run() {
+        this.disp.setImage(this.noeud.calculer(this.calcul), this.calcul.getX(), this.calcul.getY());
+    }
 
 }
